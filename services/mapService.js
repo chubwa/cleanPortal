aa6789c4302dbbf527bbd2443cf9f5d3f074a093
Leonard Mpande <leo.august27@gmail.com>
2016-01-06 00:40:26 +0300

    change legends boundary

--- services/mapService.js
+++ services/mapService.js
@@ -311,7 +311,6 @@ angular.module("hmisPortal")
         angular.forEach(max_and_min[2],function(valueL,indexL){
             if(valueTouseArray[valueIndex].id==valueL.id){
                 i++;
-
                 if(valueL.value!=0&&valueL.value>=max_and_min[0]){
 
                     legend[2].members=legend[2].members+1;
@@ -319,7 +318,7 @@ angular.module("hmisPortal")
                     return false;
                 }
 
-                if(valueL.value!=0&&valueL.value>max_and_min[0]&&valueL.value<max_and_min[1]){
+                if(valueL.value!=0&&valueL.value<=((max_and_min[1]+max_and_min[0])/2)&&valueL.value>max_and_min[1]){
 
                     legend[1].members=legend[1].members+1;
                     classfy = legend[1];
@@ -332,10 +331,28 @@ angular.module("hmisPortal")
                     return false;
                 }
 
-                legend[0].members= i;
-                classfy = legend[0];
 
+//
+//                if(valueL.value!=0&&valueL.value>=max_and_min[0]){
+//
+//                    legend[2].members=legend[2].members+1;
+//                    classfy = legend[2];
+//                    return false;
+//                }else
+//                if(valueL.value!=0&&valueL.value<=((max_and_min[1]+max_and_min[0])/2)&&valueL.value>max_and_min[1]){
+//
+//                    legend[1].members=legend[1].members+1;
+//                    classfy = legend[1];
+//                    return false;
+//                }else
+//                if(valueL.value==0||valueL.value<=max_and_min[1]){
+//                    legend[0].members=legend[0].members+1;
+//                    classfy = legend[0];
+//                    return false;
+//                }
 
+//                legend[0].members= i;
+//                classfy = legend[0];
             }else{
 
                 return false;
