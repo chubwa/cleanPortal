47055571b751531f58980bf6b44c06dbd31e3866
chubwa <tuzoengelbert@gmail.com>
2016-01-07 02:06:09 +0300

    Adding content to RCH indicators

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -149,7 +149,62 @@ angular.module("hmisPortal")
             templateUrl: 'views/cervicalCancer.html',
             controller: 'cervicalCancerCtrl'
         });
-
+        $routeProvider.when("/book10",{
+            templateUrl: 'views/book10.html',
+            controller: 'book10Ctrl'
+        });
+        $routeProvider.when("/death",{
+            templateUrl: 'views/death.html',
+            controller: 'deathCtrl'
+        });
+        $routeProvider.when("/sti",{
+            templateUrl: 'views/sti.html',
+            controller: 'stiCtrl'
+        });
+        $routeProvider.when("/tracer",{
+            templateUrl: 'views/tracer.html',
+            controller: 'tracerCtrl'
+        });
+        $routeProvider.when("/dtc",{
+            templateUrl: 'views/dtc.html',
+            controller: 'dtcCtrl'
+        });
+        $routeProvider.when("/hbc",{
+            templateUrl: 'views/hbc.html',
+            controller: 'hbcCtrl'
+        });
+        $routeProvider.when("/eye",{
+            templateUrl: 'views/eye.html',
+            controller: 'eyeCtrl'
+        });
+        $routeProvider.when("/hct",{
+            templateUrl: 'views/hct.html',
+            controller: 'hctCtrl'
+        });
+        $routeProvider.when("/pmtct",{
+            templateUrl: 'views/pmtct.html',
+            controller: 'pmtctCtrl'
+        });
+        $routeProvider.when("/lep07",{
+            templateUrl: 'views/lep07.html',
+            controller: 'lep07Ctrl'
+        });
+        $routeProvider.when("/lep09",{
+            templateUrl: 'views/lep09.html',
+            controller: 'lep09Ctrl'
+        });
+        $routeProvider.when("/lep10",{
+            templateUrl: 'views/lep10.html',
+            controller: 'lep10Ctrl'
+        });
+        $routeProvider.when("/tbo7",{
+            templateUrl: 'views/tbo7.html',
+            controller: 'tbo7Ctrl'
+        });
+        $routeProvider.when("/tb09",{
+            templateUrl: 'views/tb09.html',
+            controller: 'tb09Ctrl'
+        });
         $routeProvider.otherwise({
             redirectTo: '/home'
         });
